c64243f0d1b7334f392386e3c4f81331691b1349
Banban <alban.rouault@hotmail.com>
2023-03-26 21:34:29 +0200

    Ajout de plusieurs éléments concernant la vue

---
+++ src/view/_gabarit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!-- Head -->
+<?php require_once VIEW.'element/head.php' ?>
+
+<body>
+    <!-- Header -->
+    <?php require_once VIEW.'element/headerClient.php' ?>
+
+    <!-- Content -->
+    <?php echo $contentPage ?>
+
+    <!-- Footer -->
+    <?php require_once VIEW.'element/footer.php' ?>
+</body>
+
+</html>
