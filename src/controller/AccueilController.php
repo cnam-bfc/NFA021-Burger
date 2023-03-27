c2f08c74c50b010bf61c1f8f3632771b1a862ead
Banban <alban.rouault@hotmail.com>
2023-03-27 21:22:51 +0200

    L2WEB-91 : Accueil html/css fini

--- src/controller/AccueilController.php
+++ src/controller/AccueilController.php
@@ -18,9 +18,16 @@ class AccueilController
             ["nom" => "triple cheese", "img" => IMG . "recette/burger/triple_cheese.webp"]
         );
 
+        // voir si on fait défiler différentes news 
+        $news = array (
+            "title" => "Notre histoire",
+            "message" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+        );
+
         $data = array(
             "backgroundImage" => $backgroundImage,
-            "topRecette" => $topRecette
+            "topRecette" => $topRecette,
+            "news" => $news
         );
         
         $view = new View('AccueilClientView');
--- src/view/AccueilClientView.php
+++ src/view/AccueilClientView.php
@@ -1,6 +1,6 @@
-<div class="WrapperColonne">
-    <img src=<?php echo $backgroundImage?>>
-    <div id="top_recette">
+<div class="wrapper_colonne" id="wrapper_accueil">
+    <img src=<?php echo $backgroundImage?> class="accueil_margin">
+    <div class="accueil_margin" id="top_recette">
         <div class="top_recette_accueil_div">
             <img src=<?php echo $topRecette[0]["img"]?>>
             <p><?php echo $topRecette[0]["nom"]?></p>
@@ -14,12 +14,12 @@
             <img src=<?php echo $topRecette[2]["img"]?>>
         </div>
     </div>
-    <div>
-        <h2></h2>
-        <img>
+    <div class="wrapper_colonne_center background_primaire accueil_margin">
+        <h2 class="color_secondaire bold">Nos restaurants</h2>
+        <img src="<?php echo IMG."carte_with_ping_name.png" ?>" id="accueil_map">
     </div>
-    <article>
-        <h2></h2>
-        <p></p>
+    <article class="wrapper_colonne_center accueil_margin">
+        <h2 class="color_secondaire bold"><?php echo $news["title"];?></h2>
+        <p class ="text-justify"><?php echo $news["message"];?></p>
     </article>
 </div>
