fbdcc8db4583e48b08ce91d34463103cc848096d
Banban <alban.rouault@hotmail.com>
2023-03-28 21:32:28 +0200

    Avancement de la partie js pour l'accueil (voir ou placer le js)

---
+++ src/view/element/Head.php
@@ -0,0 +1,9 @@
+<head>
+    <title>NFA021-Burger</title>
+    <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="<?php echo CSS ?>styles.css">
+    <link href="<?php echo ASSETS ?>fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="<?php echo ASSETS ?>fontawesome/css/brands.css" rel="stylesheet">
+    <link href="<?php echo ASSETS ?>fontawesome/css/solid.css" rel="stylesheet">
+</head>
