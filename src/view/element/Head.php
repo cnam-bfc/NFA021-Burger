8b64252fbee181567174f2a7e79e4810f86a40d0
Banban <alban.rouault@hotmail.com>
2023-03-30 20:41:53 +0200

    ajout bibliothèque pdf

--- src/view/element/Head.php
+++ src/view/element/Head.php
@@ -2,6 +2,7 @@
     <title>NFA021-Burger</title>
     <meta charset="utf-8">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="<?php ASSETS?>jspdf/jspdf.umd.min"></script> <!-- bibliothèque pour générer un pdf -->
     <link rel="stylesheet" href="<?php echo CSS ?>styles.css">
     <link href="<?php echo ASSETS ?>fontawesome/css/fontawesome.css" rel="stylesheet">
     <link href="<?php echo ASSETS ?>fontawesome/css/brands.css" rel="stylesheet">
