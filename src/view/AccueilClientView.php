313ef656d6e25a4d3734381f726e47bb68f82be7
Banban <alban.rouault@hotmail.com>
2023-03-30 20:41:27 +0200

    Correction probème margin page accueil client

--- src/view/AccueilClientView.php
+++ src/view/AccueilClientView.php
@@ -4,7 +4,7 @@
 <!-- On ajoute la feuille de js associé à la page -->
 <script src="<?php echo JS?>AccueilClient.js"></script>
 
-<div class="wrapper_colonne" id="wrapper_accueil">
+<div class="wrapper_colonne_whithout_margin" id="wrapper_accueil">
     <img src=<?php echo $backgroundImage ?> class="accueil_margin">
     <div class="accueil_margin" id="top_recette">
         <div class="top_recette_accueil_div">
