41ccc74ce009511c4096a70922b1420afe66186f
Banban <alban.rouault@hotmail.com>
2023-03-31 22:32:47 +0200

    Modification de la page d'erreur

--- src/view/ErrorView.php
+++ src/view/ErrorView.php
@@ -1,6 +1,8 @@
 <!-- Ajout du css associé à la page -->
 <link rel="stylesheet" href="<?= CSS ?>Error.css">
 
-<div class="wrapper_colonne_center">
-    <h1 id="titre" class="titre"><?= $code . ' - ' . $message ?></h1>
+<div class="wrapper axe_colonne second_axe_center margin_bottom_top_large ">
+    <img src="<?= IMG ?>ingredient/coupe/top_burger.webp" alt="404" id="error_top">
+    <h1 id="error_message" class= "bold font_size_large"><?= $code . ' - ' . $message ?></h1>
+    <img src="<?= IMG ?>ingredient/coupe/bot_burger.webp" alt="404" id="error_bottom">
 </div>
