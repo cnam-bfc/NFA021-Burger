e9664d0b35768dddd1f547079dcc380ce12a2b2c
Tarek AHMED BELHADJ <tarekahmedbelhadj@gmail.com>
2023-04-05 08:46:21 +0200

    Salut2

--- src/Router.php
+++ src/Router.php
@@ -33,9 +33,11 @@ class Router
         'gerant/recettes/supprimer' => ["controller" => "EditRecetteController", "method" => "renderViewSupprimerRecette"],
         
         //BDC
-        'gerant/nouveauproduit' => ["controller" => "NouveauProduitController", "method" => "renderView"],
         'gerant/listebdc' => ["controller" => "ListeBDCController", "method" => "renderView"],
         'gerant/nouveaubdc' => ["controller" => "NouveauBDCController", "method" => "renderView"],
+
+        //Produits
+        'gerant/nouveauproduit' => ["controller" => "NouveauProduitController", "method" => "renderView"],
         'gerant/listeproduits' => ["controller" => "ListeProduitsController", "method" => "renderView"],
     ];
 
