df061a389c2ecca01b3acc1dac4860b910d50e14
Nathan <nathan2506@hotmail.fr>
2023-04-08 11:59:47 +0200

    salut

--- src/view/AccueilEmployeView.php
+++ src/view/AccueilEmployeView.php
@@ -1,31 +1,24 @@
 <link rel="stylesheet" href="<?php echo CSS ?>AccueilEmploye.css">
 
-<div class="wrapper axe_colonne margin_left_right_extreme_large main_axe_space_around grow" id="wrapper_accueil">
-    <div id="espace_gerant" class="wrapper axe_ligne wrap width_auto height_50 main_axe_center background_quinnaire">
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
+<div class="wrapper axe_colonne main_axe_space_around grow" id="wrapper_accueil">
+    <div id="espace_gerant" class="wrapper axe_ligne wrap width_auto height_50 main_axe_space_around main_axe_center background_quinnaire">
+        <h1 class="width_100 margin_very_large">Espace Gérant</h1><br>
+        <div class="gerant_box margin_very_large"></div>
+        <div class="gerant_box margin_very_large"></div>
+        <div class="gerant_box margin_very_large"></div>
+        <div class="gerant_box margin_very_large"></div>
+        <div class="gerant_box margin_very_large"></div>
+        <div class="gerant_box margin_very_large"></div>
+        <div class="gerant_box margin_very_large"></div>
     </div>
-    <div id="espace_cuisinier" class="wrapper axe_ligne wrap width_auto height_25 main_axe_center background_tertiaire">
-        <div>
-        </div>
+    <div id="espace_cuisinier" class="wrapper axe_ligne wrap width_auto height_25 main_axe_space_around main_axe_center background_tertiaire">
+        <h1 class="margin_very_large">Espace Cuisinier</h1>
+        <div class="cuisinier_box margin_very_large"><br></div>
     </div>
-    <div id="espace_livreur" class="wrapper axe_ligne wrap width_auto height_100 main_axe_center background_secondaire">
-        <div>
-        </div>
-        <div>
-        </div>
+    <div id="espace_livreur" class="wrapper axe_ligne wrap width_auto height_100 main_axe_space_around main_axe_center background_secondaire">
+        <h1 class="margin_very_large">Espace Livreur</h1>
+        <div class="livreur_box margin_very_large"></div>
+        <div class="livreur_box margin_very_large"></div>
     </div>
 
 </div>
