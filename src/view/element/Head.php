d9d4ba6216e6aa11dd07b1ce5888cd2b076a4ffc
Banban <alban.rouault@hotmail.com>
2023-04-12 20:11:45 +0200

    Ajout de commentaire dans le head

--- src/view/element/Head.php
+++ src/view/element/Head.php
@@ -1,15 +1,7 @@
 <head>
     <title>Cha'bu</title>
+    <!-- Meta tags -->
     <meta charset="utf-8">
-    <script src="<?php echo ASSETS ?>jquery/jquery-3.6.4.min.js"></script>
-    <script src="<?php echo ASSETS ?>jspdf/jspdf.umd.min"></script> <!-- bibliothèque pour générer un pdf -->
-    <link href="<?php echo ASSETS ?>fontawesome/css/fontawesome.css" rel="stylesheet">
-    <link href="<?php echo ASSETS ?>fontawesome/css/brands.css" rel="stylesheet">
-    <link href="<?php echo ASSETS ?>fontawesome/css/solid.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo CSS ?>element/Reset.css">
-    <link rel="stylesheet" href="<?php echo CSS ?>element/Header.css">
-    <link rel="stylesheet" href="<?php echo CSS ?>element/Footer.css">
-    <link rel="stylesheet" href="<?php echo CSS ?>styles.css">
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <meta name="viewport" content="width=500, initial-scale=1">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
@@ -18,4 +10,15 @@
     <meta name="owner" content="CNAM">
     <meta name="author" content="CNAM">
     <meta name="copyright" content="CNAM">
+    <!-- Feuilles et bibliothèques JS générales-->
+    <script src="<?php echo ASSETS ?>jquery/jquery-3.6.4.min.js"></script>
+    <script src="<?php echo ASSETS ?>jspdf/jspdf.umd.min"></script> <!-- bibliothèque pour générer un pdf -->
+    <!-- Feuilles et bibliothèques CSS générales -->
+    <link href="<?php echo ASSETS ?>fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="<?php echo ASSETS ?>fontawesome/css/brands.css" rel="stylesheet">
+    <link href="<?php echo ASSETS ?>fontawesome/css/solid.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo CSS ?>element/Reset.css">
+    <link rel="stylesheet" href="<?php echo CSS ?>element/Header.css">
+    <link rel="stylesheet" href="<?php echo CSS ?>element/Footer.css">
+    <link rel="stylesheet" href="<?php echo CSS ?>styles.css">
 </head>
