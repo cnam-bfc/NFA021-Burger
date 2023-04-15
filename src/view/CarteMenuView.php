bfdd936c470f7a19adf37e499c0670aad40d545b
Nathan <nathan2506@hotmail.fr>
2023-04-15 11:05:54 +0200

    salut

--- src/view/CarteMenuView.php
+++ src/view/CarteMenuView.php
@@ -1,10 +1,1 @@
 <link rel="stylesheet" href="<?php echo CSS ?>CarteMenu.css">
-
-<div class="wrapper axe_ligne wrapper main_axe_space_between main_axe_center background_secondaire grow margin_carte">
-    <div class="box wrapper axe_colonne margin_large main_axe_center second_axe_center">
-        <a href="http://localhost/NFA021-Burger/public/gerant/statistiques" class="wrapper wrap axe_colonne main_axe_space_evenly main_axe_center second_axe_center grow">
-            <img class='img'>
-            <h2 class="bold">Statistiques</h2>
-        </a>
-    </div>
-</div>
