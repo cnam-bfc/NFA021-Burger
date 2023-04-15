b14de4b8cb6fab1ed733b450dc6a41a50e20ae24
Banban <alban.rouault@hotmail.com>
2023-04-15 11:20:02 +0200

    Suppression d'un commentaire inutile dans accueilController.php

--- src/controller/AccueilController.php
+++ src/controller/AccueilController.php
@@ -28,35 +28,11 @@ class AccueilController extends Controller
             "message" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
         );
 
-        // DEBUT - A DEPLACER DANS AJAX
-        // ici il faudra appeler le modèle pour récupérer le top 3 des recettes du moment mais c'est une démonstration
-        $recetteDAO = new RecetteDAO();
-        $topRecettes = $recetteDAO->selectTop3Recette();
-        if ($topRecettes !== null) {
-            $topRecette = array();
-            foreach ($topRecettes as $recette) {
-                $topRecette[] = array(
-                    "nom" => $recette->getNom(),
-                    "img" => IMG . $recette->getPhotoRecette()
-                );
-            }
-            $view->topRecette = $topRecette;
-        } else {
-            $view->topRecette = array(
-                ["nom" => "cheddar lover", "img" => IMG . "recette/burger/cheddar_lover.webp"],
-                ["nom" => "steakhouse", "img" => IMG . "recette/burger/steakhouse.webp"],
-                ["nom" => "triple cheese", "img" => IMG . "recette/burger/triple_cheese.webp"]
-            );
-        }
-        // FIN - A DEPLACER DANS AJAX
-
         $view->renderView();
     }
 
     public function refreshTopRecetteAJAX()
     {
-        $view = new View(BaseTemplate::JSON);
-
         $recetteDAO = new RecetteDAO();
         $topRecettes = $recetteDAO->selectTop3Recette();
         if ($topRecettes !== null) {
@@ -82,6 +58,8 @@ class AccueilController extends Controller
                 "image" => IMG . "recette/burger/triple_cheese.webp"
             );
         }
+        $view = new View(BaseTemplate::JSON);
+
         $view->json = $result;
 
         $view->renderView();
