9a7c7dbe9e02dd44b8b825bc052913f1fa658cd4
Nathan <nathan2506@hotmail.fr>
2023-04-25 17:11:09 +0200

    Page CarteMenu

--- src/controller/CarteMenuController.php
+++ src/controller/CarteMenuController.php
@@ -5,6 +5,13 @@ class CarteMenuController extends Controller
     public function renderView()
     {
         $view = new View(BaseTemplate::CLIENT, 'CarteMenuView');
+
+        $view->image_burger = array(
+            ["nom" => "cheddar lover", "img" => IMG . "recette/burger/cheddar_lover.webp"],
+            ["nom" => "steakhouse", "img" => IMG . "recette/burger/steakhouse.webp"],
+            ["nom" => "triple cheese", "img" => IMG . "recette/burger/triple_cheese"],
+            );
+
         $view->renderView();
     }
 }
