35b3876d0641d2a200e2a7d20aed31ca492f0afe
FarmVivi <farmvividev@gmail.com>
2023-04-30 14:00:56 +0200

    ErrorController - Simplification

--- src/controller/ErrorController.php
+++ src/controller/ErrorController.php
@@ -3,21 +3,18 @@ class ErrorController extends Controller
 {
     public static function error($code, $message, $renderView = true)
     {
+        // Définition du code d'erreur HTTP
+        header("HTTP/1.0 " . $code . " " . $message);
+
         if ($renderView) {
             $controller = new ErrorController();
             $controller->renderView($code, $message);
-        } else {
-            // Définition du code d'erreur HTTP
-            header("HTTP/1.0 " . $code . " " . $message);
         }
     }
 
-    public function renderView($code, $message)
+    private function renderView($code, $message)
     {
         $view = new View(BaseTemplate::EMPTY, 'ErrorView');
-
-        // Définition du code d'erreur HTTP
-        header("HTTP/1.0 " . $code . " " . $message);
 
         // Définition des variables utilisées dans la vue
         $view->code = $code;
