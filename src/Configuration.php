0cd6ca3592468a6628bf75bef99fd4da52343bef
FarmVivi <farmvividev@gmail.com>
2023-04-30 20:01:35 +0200

    Configuration - Get and Set properties

--- src/Configuration.php
+++ src/Configuration.php
@@ -6,7 +6,7 @@ class Configuration
     public static function getInstance()
     {
         if (is_null(self::$instance)) {
-            self::$instance = new Configuration();
+            self::createInstance();
         }
         return self::$instance;
     }
@@ -16,46 +16,178 @@ class Configuration
         self::$instance = new Configuration();
     }
 
+    const CATEGORY_BDD = 'bdd';
+
     private $config = null;
 
     private function __construct()
     {
         if (!file_exists(DATA_FOLDER . 'config.json')) {
-            throw new Exception('Le fichier de configuration n\'existe pas, veuillez copier le fichier config.exemple.json et le renommer en config.json dans le dossier data');
+            throw new Exception('Le fichier de configuration n\'existe pas');
         }
         $this->config = json_decode(file_get_contents(DATA_FOLDER . 'config.json'), true);
+    }
+
+    /**************************
+     *** DEBUT - GÉNÉRALITÉ ***
+     **************************/
+    /**
+     * Fonction permettant de savoir si une valeur de configuration existe
+     * 
+     * @param string $category (catégorie de la valeur de configuration)
+     * @param string $key (clé de la valeur de configuration)
+     * @return bool (true si la clé existe, false sinon)
+     */
+    private function has($category, $key)
+    {
+        return isset($this->config[$category][$key]);
+    }
+
+    /**
+     * Fonction permettant de récupérer une valeur de configuration
+     * 
+     * @param string $category (catégorie de la valeur de configuration)
+     * @param string $key (clé de la valeur de configuration)
+     * @return mixed (valeur de configuration)
+     * @throws Exception (si la valeur de configuration n'existe pas)
+     */
+    private function get($category, $key)
+    {
+        if (!$this->has($category, $key)) {
+            throw new Exception('La valeur de configuration \'' . $key . '\' n\'existe pas dans la catégorie \'' . $category . '\'');
+        }
+        return $this->config[$category][$key];
+    }
+
+    /**
+     * Fonction permettant de définir une valeur de configuration
+     * 
+     * @param string $category (catégorie de la valeur de configuration)
+     * @param string $key (clé de la valeur de configuration)
+     * @param mixed $value (valeur de configuration)
+     * @throws Exception (si une erreur survient lors de l'écriture dans le fichier de configuration)
+     */
+    private function set($category, $key, $value)
+    {
+        $this->config[$category][$key] = $value;
+        $config_json = json_encode($this->config, JSON_PRETTY_PRINT);
+        $success = file_put_contents(DATA_FOLDER . 'config.json', $config_json);
+        if (!$success) {
+            throw new Exception('Une erreur est survenue lors de l\'écriture dans le fichier de configuration');
+        }
     }
 
     /*******************************
      *** DEBUT - BASE DE DONNEES ***
      ******************************/
+    /**
+     * Méthode permettant de récupérer l'hôte de la base de données
+     * 
+     * @return string (hôte de la base de données)
+     * @throws Exception (si la valeur de configuration n'existe pas)
+     */
     public function getBddHost()
     {
-        return $this->config['bdd']['host'];
+        return $this->get(self::CATEGORY_BDD, 'host');
     }
 
-    public function getBddName()
-    {
-        return $this->config['bdd']['database'];
-    }
-
-    public function getBddUser()
-    {
-        return $this->config['bdd']['user'];
-    }
-
-    public function getBddPassword()
-    {
-        return $this->config['bdd']['password'];
-    }
-
+    /**
+     * Méthode permettant de récupérer le port de la base de données
+     * 
+     * @return string (port de la base de données)
+     * @throws Exception (si la valeur de configuration n'existe pas)
+     */
     public function getBddPort()
     {
-        if (isset($this->config['bdd']['port'])) {
-            return $this->config['bdd']['port'];
-        } else {
-            return 3306;
-        }
+        return $this->get(self::CATEGORY_BDD, 'port');
+    }
+
+    /**
+     * Méthode permettant de récupérer le nom de la base de données
+     * 
+     * @return string (nom de la base de données)
+     * @throws Exception (si la valeur de configuration n'existe pas)
+     */
+    public function getBddName()
+    {
+        return $this->get(self::CATEGORY_BDD, 'database');
+    }
+
+    /**
+     * Méthode permettant de récupérer l'utilisateur de la base de données
+     * 
+     * @return string (utilisateur de la base de données)
+     * @throws Exception (si la valeur de configuration n'existe pas)
+     */
+    public function getBddUser()
+    {
+        return $this->get(self::CATEGORY_BDD, 'user');
+    }
+
+    /**
+     * Méthode permettant de récupérer le mot de passe de la base de données
+     * 
+     * @return string (mot de passe de la base de données)
+     * @throws Exception (si la valeur de configuration n'existe pas)
+     */
+    public function getBddPassword()
+    {
+        return $this->get(self::CATEGORY_BDD, 'password');
+    }
+
+    /**
+     * Méthode permettant de modifier l'hôte de la base de données
+     * 
+     * @param string $host (hôte de la base de données)
+     * @throws Exception (si une erreur survient lors de l'écriture dans le fichier de configuration)
+     */
+    public function setBddHost($host)
+    {
+        $this->set(self::CATEGORY_BDD, 'host', $host);
+    }
+
+    /**
+     * Méthode permettant de modifier le port de la base de données
+     * 
+     * @param string $port (port de la base de données)
+     * @throws Exception (si une erreur survient lors de l'écriture dans le fichier de configuration)
+     */
+    public function setBddPort($port)
+    {
+        $this->set(self::CATEGORY_BDD, 'port', $port);
+    }
+
+    /**
+     * Méthode permettant de modifier le nom de la base de données
+     * 
+     * @param string $database (nom de la base de données)
+     * @throws Exception (si une erreur survient lors de l'écriture dans le fichier de configuration)
+     */
+    public function setBddName($database)
+    {
+        $this->set(self::CATEGORY_BDD, 'database', $database);
+    }
+
+    /**
+     * Méthode permettant de modifier l'utilisateur de la base de données
+     * 
+     * @param string $user (utilisateur de la base de données)
+     * @throws Exception (si une erreur survient lors de l'écriture dans le fichier de configuration)
+     */
+    public function setBddUser($user)
+    {
+        $this->set(self::CATEGORY_BDD, 'user', $user);
+    }
+
+    /**
+     * Méthode permettant de modifier le mot de passe de la base de données
+     * 
+     * @param string $password (mot de passe de la base de données)
+     * @throws Exception (si une erreur survient lors de l'écriture dans le fichier de configuration)
+     */
+    public function setBddPassword($password)
+    {
+        $this->set(self::CATEGORY_BDD, 'password', $password);
     }
 
     /*****************************
