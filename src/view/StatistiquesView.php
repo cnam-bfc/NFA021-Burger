5919d9c2140c15ad9a80f73cd7028c5d35d09c91
Banban <alban.rouault@hotmail.com>
2023-05-16 16:30:39 +0200

    Avancement statistiques

--- src/view/StatistiquesView.php
+++ src/view/StatistiquesView.php
@@ -17,39 +17,83 @@
             <div data_id="ongletSelection">
                 <div class="box_graphe">
                     <h3 class="graphe_categorie bold">Ventes</h3>
-                    <p class="graphe_choix"><a href="#">Burger</a>
-                    <p>
-                    <p class="graphe_choix"><a href="#">Ingedient</a></p>
+                    <button class="graphe_choix">
+                        <p>Burger</p>
+                    </button>
+                    <button class="graphe_choix">
+                        <p>Ingedient</p>
+                    </button>
                 </div>
 
                 <div class="box_graphe">
                     <h3 class="graphe_categorie bold">Achats</h3>
-                    <p class="graphe_choix"><a href="#">Tous les produits</a>
-                    <p>
-                    <p class="graphe_choix"><a href="#">Choix d'un produit</a></p>
+                    <button class="graphe_choix">
+                        <p>Tous les produits</p>
+                    </button>
+                    <button class="graphe_choix">
+                        <p>Choix d'un produit</p>
+                    </button>
                 </div>
 
                 <div class="box_graphe">
                     <h3 class="graphe_categorie bold">Généralités</h3>
-                    <p class="graphe_choix"><a href="#">Bénéfices</a>
-                    <p>
-                    <p class="graphe_choix"><a href="#">Nombre de client</a></p>
+                    <button class="graphe_choix">
+                        <p>Bénéfices</p>
+                    </button>
+                    <button class="graphe_choix">
+                        <p>Nombre de client</p>
+                    </button>
                 </div>
             </div>
 
             <div data_id="ongletConfiguration">
-
+                <div class="box_graphe">
+                    <h3 class="graphe_categorie bold">Nom</h3>
+                    <input type="text" id="nom_graphe" name="nom_graphe" class="input" placeholder="Nom du graphique">
+                </div>
+                <div class="box_graphe">
+                    <h3 class="graphe_categorie bold">Description</h3>
+                    <textarea id="description_graphe" name="description_graphe" class="textarea" placeholder="Description du graphique" minlength=0 maxlength=250></textarea>
+                </div>
+                <div class="box_graphe">
+                    <h3 class="graphe_categorie bold">Type de graphe</h3>
+                    <select id="type_graphe" class="select">
+                        <option value="" selected>Choisissez un type de graphique</option>
+                    </select>
+                </div>
+                <div class="box_graphe">
+                    <h3 class="graphe_categorie bold">Ordre</h3>
+                    <select id="ordre_graphe" class="select">
+                        <option value="" selected>Choisissez l'ordre de votre graphique</option>
+                    </select>
+                </div>
             </div>
-
             <div data_id="ongletParametrage">
-
+                <div class="box_graphe">
+                    <h3 class="graphe_categorie bold">Date</h3>
+                    <span class="wrapper axe_ligne main_axe_space_between second_axe_center grow">
+                        <label for="choix_date_graphe">Date personnalisé</label>
+                        <input class="input" id="choix_date_graphe" name="choix_date_graphe" type="checkbox">
+                    </span>
+                    <span class="wrapper axe_ligne main_axe_space_between second_axe_center grow">
+                        <label for="date_debut_graphe">Date début</label>
+                        <input type="date" id="date_debut_graphe" name="date_debut_graphe" class="input" placeholder="Nom du graphique">
+                    </span>
+                    <span class="wrapper axe_ligne main_axe_space_between second_axe_center grow">
+                        <label for="date_fin_graphe">Date fin</label>
+                        <input type="date" id="date_fin_graphe" name="date_fin_graphe" class="input" placeholder="Nom du graphique">
+                    </span>
+                </div>
+                <div class="box_graphe">
+                    <h3 class="graphe_categorie bold" id="spécificité graphe">Spécificité</h3>
+                </div>
             </div>
         </div>
         <div id="boutons_graphe" class="wrapper axe_ligne gap_left_right_moyen">
-            <button title="Ajouter le graphe" id="add_graphe" class="bouton"><i class="fa-solid fa-plus fa-xl"></i></button>
-            <button title="Sauvegarder les changements" id="save_graphe" class="bouton"><i class="fa-solid fa-floppy-disk fa-xl"></i></button>
-            <button title="Annuler les changements" id="cancel_graphe" class="bouton"><i class="fa-solid fa-xmark fa-xl"></i></button>
-            <button title="Supprimer le graphe" id="delete_graphe" class="bouton"><i class="fa-solid fa-trash fa-xl"></i></button>
+            <button title="Ajouter le graphe" id="add_graphe" class="bouton"><i class="fa-solid fa-plus fa-lg"></i></button>
+            <button title="Sauvegarder les changements" id="save_graphe" class="bouton"><i class="fa-solid fa-floppy-disk fa-lg"></i></button>
+            <button title="Annuler les changements" id="cancel_graphe" class="bouton"><i class="fa-solid fa-xmark fa-lg"></i></button>
+            <button title="Supprimer le graphe" id="delete_graphe" class="bouton"><i class="fa-solid fa-trash fa-lg"></i></button>
         </div>
     </section>
     <section class="wrapper axe_colonne second_axe_center width_100 main_axe_space_between" id="contenu">
@@ -57,7 +101,7 @@
             <h1>Statistiques</h1>
         </div>
 
-        <div>Le graphe</div>
+        <div id="graphes">Le graphe</div>
 
         <div class="center_items_horizontal">
             <button class="bouton bouton_primaire margin_bottom_top_large">Exporter</button>
