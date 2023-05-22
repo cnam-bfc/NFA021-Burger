07efc7c7515b11549d37b7249de2bbd4a5a9d73b
Nathan <nathan2506@hotmail.fr>
2023-05-22 21:38:05 +0200

    EcranCuisine

--- public/assets/js/Panier.js
+++ public/assets/js/Panier.js
@@ -2,7 +2,7 @@ const panier = document.getElementById('Panier');
 function showData() {
 
     panier.innerHTML = $_SESSION['panier'];
-    for (let index = 0; index < <?php echo $_SESSION['panier']['ingredientsFinaux'].length?>; index + 2) {
+    for (let index = 0; index < <?php echo $_SESSION['panier']['ingredientsFinaux'].length?>; + index + 2) {
         let p = document.createElement("p");
         p.textContent = <?php echo $_SESSION["panier"]['ingredientsFinaux'][index]; ?>;
         panier.appendChild();
--- src/view/EcranCuisineView.php
+++ src/view/EcranCuisineView.php
@@ -3,47 +3,6 @@
 
 
 <div class="wrapper axe_ligne wrap main_axe_center second_axe_center grow" id="wrapper_cuisine">
-    <div class="commande focus">
-        <div class="num_com"><p>001</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="wrapper axe_colonne wrap commande">
-        <div class="num_com height_25"><p>002</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="commande">
-        <div class="num_com"><p>003</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="commande">
-        <div class="num_com"><p>004</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="commande">
-        <div class="num_com"><p>005</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="commande">
-        <div class="num_com"><p>006</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="commande">
-        <div class="num_com"><p>007</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-    <div class="commande">
-        <div class="num_com"><p>008</p></div>
-        <div class="composition_com"></div>
-        <div class="temps_com"><p>300</p></div>
-    </div>
-
 </div>
 <div class="bouton">
     <button id="boutonValide">Valider</button>
