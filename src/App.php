af9edc0677f5dbc12d2ea9401cd67d90138a6483
FarmVivi <farmvividev@gmail.com>
2023-05-23 19:47:33 +0200

    Footer - Version include last commit id

--- src/App.php
+++ src/App.php
@@ -2,4 +2,39 @@
 class App
 {
     const VERSION = "0.1.0";
+
+    /**
+     * Méthode permettant de récupérer la version de git
+     * 
+     * @return string
+     */
+    public static function getGitVersion()
+    {
+        $lastCommit = __DIR__ . "/../.git/ORIG_HEAD";
+        $lastCommit = realpath($lastCommit);
+        if (!file_exists($lastCommit)) {
+            return "";
+        }
+
+        $hash = file_get_contents($lastCommit);
+        $hash = substr($hash, 0, 7);
+
+        return $hash;
+    }
+
+    /**
+     * Méthode permettant de récupérer la version de l'application
+     * 
+     * @return string
+     */
+    public static function getAppVersion()
+    {
+        $version = App::VERSION;
+        $gitVersion = App::getGitVersion();
+        if ($gitVersion != "") {
+            $version .= " - " . $gitVersion;
+        }
+
+        return $version;
+    }
 }
--- src/view/element/FooterEmploye.php
+++ src/view/element/FooterEmploye.php
@@ -1,6 +1,6 @@
 <footer>
     <div id="footer_bot">
         <p>Mentions légales</p>
-        <p>Version <?php echo App::VERSION; ?></p>
+        <p>Version <?php echo App::getAppVersion(); ?></p>
     </div>
 </footer>
