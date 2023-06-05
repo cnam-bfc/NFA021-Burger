a9124681977419d06f4891738a33ca81cc4204c6
FarmVivi <farmvividev@gmail.com>
2023-06-05 13:30:09 +0200

    Itinéraire - Suivi position

--- public/assets/js/Itineraire.js
+++ public/assets/js/Itineraire.js
@@ -8,4 +8,17 @@ $(function () {
     L.easyButton('fa-arrow-left', function (btn, map) {
         window.location.href = "livraisons";
     }, "Retour à la liste des livraisons").addTo(map);
+
+    // Ajout du marqueur pour la position actuelle
+    let currentLocationMarker = L.marker([0, 0]).addTo(map);
+
+    // Fonction de mise à jour de la position du marqueur
+    function updateCurrentLocation(position) {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        currentLocationMarker.setLatLng([lat, lng]);
+    }
+
+    // Suivi de la position de l'utilisateur
+    navigator.geolocation.watchPosition(updateCurrentLocation);
 });
