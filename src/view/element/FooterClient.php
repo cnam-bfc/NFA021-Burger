badf09ed7a2ae2709318fb28e3362e18e647e363
Banban <alban.rouault@hotmail.com>
2023-06-06 11:27:30 +0200

    Maj footer

--- src/view/element/FooterClient.php
+++ src/view/element/FooterClient.php
@@ -1,16 +1,16 @@
 <footer>
     <div id="footer_top">
         <div class="footer_information">
-            <p>Informations complémentaires</p>
-            <p>Informations complémentaires</p>
+            <p>Nos partenaires</p>
+            <p>Nous contacter : contact@chabu.com</p>
         </div>
         <img src="<?= IMG ?>logo/logo.png">
         <div class="footer_information">
-            <p>Informations complémentaires</p>
-            <p>Informations complémentaires</p>
+            <p>Notre FAQ</p>
+            <p>Nous suivre sur Linkedin!</p>
         </div>
     </div>
-    <p id="footer_mid">Adresse</p>
+    <p id="footer_mid">11 Rue Georges Maugey<br>71100 Chalon-sur-Saône</p>
     <div id="footer_bot">
         <p>Mentions légales</p>
         <p>Version <?php echo App::getAppVersion(); ?></p>
