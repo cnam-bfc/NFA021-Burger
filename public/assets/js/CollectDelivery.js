835ff6e35646e46f3e78b52aa3291a5201d65542
NL <nicolas.lucas.auditeur@lecnam.net>
2023-06-17 16:58:22 +0200

    Push

--- public/assets/js/CollectDelivery.js
+++ public/assets/js/CollectDelivery.js
@@ -1,42 +1,90 @@
-$(function () {
-    const bclick_collect = document.getElementById("bclick_collect");
-    const bdelivery = document.getElementById("bdelivery");
-    const click_collect = document.getElementById("clickCollect");
-    const delivery = document.getElementById("delivery");
+$(document).ready(function () {
+    const bclick_collect = $("#bclick_collect");
+    const bdelivery = $("#bdelivery");
+    const clickcollect = $("#clickCollect");
+    const delivery = $("#delivery");
 
 
 
-    bclick_collect.addEventListener("change", () => {
-        if (bclick_collect.checked) {
-            bdelivery.checked = false;
-            delivery.style.display = "none";
-            click_collect.style.display = "block";
-        }
-        console.log(document.getElementById("emballage").value);
+    bclick_collect.on("click", function () {
+        delivery.hide();
+        clickcollect.show();
+        console.log($("#emballage").val());
     });
 
-    bdelivery.addEventListener("change", () => {
-        if (bdelivery.checked) {
-            bclick_collect.checked = false;
-
-            click_collect.style.display = "none";
-            delivery.style.display = "block";
-        }
+    bdelivery.on("click", function () {
+        clickcollect.hide();
+        delivery.show();
     });
 
-    //Pour vérifier que l'heure entrée est supérieure à l'heure actuelle.
-    console.log(new Date().toLocaleTimeString('fr-FR', { hour12: false, hour: '2-digit', minute: '2-digit' }));
+
+    // bclick_collect.trigger("click");
+    // console.log('a');
+    //////////////////////////////////////////////////////
+    const element = document.getElementById("bclick_collect");
+    const customCursor = document.getElementById("custom-cursor");
+
+    //////1 CLICK
+
+    // // Création d'un nouvel événement de clic de souris
+    // const event = new MouseEvent("click", {
+    //     bubbles: true,
+    //     cancelable: true,
+    //     view: window
+    // });
+
+    // // Déclenchement de l'événement de clic de souris sur l'élément
+    // element.dispatchEvent(event);
+
+    // Simulation de clic
+    function simulateClick(event) {
+        // Obtenir les coordonnées du clic
+        const rect = element.getBoundingClientRect();
+        const x = rect.left + rect.width / 2;
+        const y = rect.top + rect.height / 2;
+
+        // Positionner l'élément du curseur personnalisé aux coordonnées du clic
+        customCursor.style.left = x + "px";
+        customCursor.style.top = y + "px";
+
+        // Afficher l'élément du curseur personnalisé
+        customCursor.style.display = "block";
+
+        // Obtenir l'élément situé aux coordonnées du clic
+        const targetElement = document.elementFromPoint(x, y);
+
+        // Vérifier si l'élément est valide et déclencher un événement de clic sur celui-ci
+        if (targetElement) {
+            targetElement.click();
+        }
+
+        // Effectuer d'autres actions liées au clic simulé
+        // ...
+    }
 
 
+    // Appel de la fonction simulateClick pour simuler le clic
+    console.log("avant carton");
+
+    /////////2CLICK
+    simulateClick();
+    /////////////
 
 });
+
+
+//Pour vérifier que l'heure entrée est supérieure à l'heure actuelle.
+console.log(new Date().toLocaleTimeString('fr-FR', { hour12: false, hour: '2-digit', minute: '2-digit' }));
+
+
+
 
 
 function valider() {
 
     console.log("valider");
 
-    if (bdelivery.checked == true) {
+    if (delivery.style.display !== "none") {
         // Si le bouton "Livraison" est coché, enregistre le mode de récupération en session
 
         const cp = document.getElementById("cp");
@@ -85,7 +133,7 @@ function valider() {
             window.location.href = 'recap';
         }
 
-    } else if (bclick_collect.checked == true) {// Si le bouton "Cliquez & Collectez" est coché, enregistre le mode de récupération en session
+    } else if (delivery.style.display == "none") {// Si le bouton "Cliquez & Collectez" est coché, enregistre le mode de récupération en session
 
         const HeureCollect = document.getElementById("heureCollect");
         const Prenom = document.getElementById("prenom");
@@ -93,21 +141,21 @@ function valider() {
 
         if (new RegExp(HeureCollect.pattern).test(HeureCollect.value) && new RegExp(Prenom.pattern).test(Prenom.value)) {
             console.log(Emballage.value);
-            
+
             var optionSelectionnee = HeureCollect.selectedOptions[0]; // Première option sélectionnée (peut en contenir plusieurs avec l'attribut 'multiple')
-            
+
             if (optionSelectionnee) {
-              var texteOptionSelectionnee = optionSelectionnee.textContent;
-              console.log("Option sélectionnée : " + texteOptionSelectionnee);
+                var texteOptionSelectionnee = optionSelectionnee.textContent;
+                console.log("Option sélectionnée : " + texteOptionSelectionnee);
             } else {
-              console.log("Aucune option sélectionnée");
+                console.log("Aucune option sélectionnée");
             }
-            
+
             const tabInfosRecup = {
                 "Mode Récupération": 'Click & Collect',
                 "Heure Collect": texteOptionSelectionnee,
                 "Prenom": Prenom.value,
-                "Emballage" : Emballage.value
+                "Emballage": Emballage.value
             };
 
             // Enregistre les informations de récupération en session
