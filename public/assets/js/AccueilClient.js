831d1d5767b7e9dbb8512b6376486635b46d385e
Banban <alban.rouault@hotmail.com>
2023-06-18 20:04:55 +0200

    Amélioration du header

--- public/assets/js/AccueilClient.js
+++ public/assets/js/AccueilClient.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
     element.removeClass('header_sticky');
     element.addClass('header_client_transparent');
     element.addClass('header_float');
+    $('.header_icone').addClass('color_not_sticky');
+    $('header li a').addClass('color_not_sticky');
 
     function changeClass() {
         if (window.scrollY == 0) {
@@ -16,15 +18,40 @@ $(document).ready(function () {
             element.addClass('header_float');
             element.removeClass('header_sticky_accueil');
             element.removeClass('header_client');
+            $('.header_icone').addClass('color_not_sticky');
+            $('header li a').addClass('color_not_sticky');
         } else {
             element.addClass('header_sticky_accueil');
             element.addClass('header_client');
             element.removeClass('header_float');
             element.removeClass('header_client_transparent');
+            $('.header_icone').removeClass('color_not_sticky');
+            $('header li a').removeClass('color_not_sticky');
         }
     }
 
     window.addEventListener('scroll', changeClass);
+
+    const header = document.querySelector('header');
+
+    // pour la couleur des texts (si on a le temps il faudrait regrouper avec celui du dessus on pourrait gagner des lignes de codes)
+    // Gestionnaire d'événement pour le survol (mouseenter)
+    header.addEventListener('mouseover', () => {
+        // Ajoutez ici les actions à effectuer lorsque le survol commence
+        if (element.hasClass('header_client_transparent')) {
+            $('header li a').removeClass('color_not_sticky');
+            $('.header_icone').removeClass('color_not_sticky');
+        }
+    });
+
+    // Gestionnaire d'événement pour la sortie du survol (mouseleave)
+    header.addEventListener('mouseout', () => {
+        // Ajoutez ici les actions à effectuer lorsque le survol se termine
+        if (element.hasClass('header_client_transparent')) {
+            $('header li a').addClass('color_not_sticky');
+            $('.header_icone').addClass('color_not_sticky');
+        }
+    });
 
     // requête AJAX pour récupérer les 3 recettes les plus populaires
     $.ajax({
@@ -47,7 +74,7 @@ $(document).ready(function () {
                 $('#top_recette>div:nth-child(1)>img').attr('src', data[1].image);
                 $('#top_recette>div:nth-child(1)>p').text(data[1].nom);
                 function clickRecette1() {
-                    window.location.href = 'visuModifsBurgers?id='+data[1].id;
+                    window.location.href = 'visuModifsBurgers?id=' + data[1].id;
                 }
                 $('#top_recette>div:nth-child(1)').on('click', clickRecette1).css('cursor', 'pointer');
                 $('#top_recette>div:nth-child(1)>img').on('click', clickRecette1).css('cursor', 'pointer');
@@ -56,7 +83,7 @@ $(document).ready(function () {
                 $('#top_recette>div:nth-child(2)>img').attr('src', data[0].image);
                 $('#top_recette>div:nth-child(2)>p').text(data[0].nom);
                 function clickRecette2() {
-                    window.location.href = 'visuModifsBurgers?id='+data[0].id;
+                    window.location.href = 'visuModifsBurgers?id=' + data[0].id;
                 }
                 $('#top_recette>div:nth-child(2)').on('click', clickRecette2).css('cursor', 'pointer');
                 $('#top_recette>div:nth-child(2)>img').on('click', clickRecette2).css('cursor', 'pointer');
@@ -65,7 +92,7 @@ $(document).ready(function () {
                 $('#top_recette>div:nth-child(3)>img').attr('src', data[2].image);
                 $('#top_recette>div:nth-child(3)>p').text(data[2].nom);
                 function clickRecette3() {
-                    window.location.href = 'visuModifsBurgers?id='+data[2].id;
+                    window.location.href = 'visuModifsBurgers?id=' + data[2].id;
                 }
                 $('#top_recette>div:nth-child(3)').on('click', clickRecette3).css('cursor', 'pointer');
                 $('#top_recette>div:nth-child(3)').css('cursor', 'pointer').css('cursor', 'pointer');
