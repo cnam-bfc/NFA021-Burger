6f010bbdf274e927f2892b68420f91f030553966
Banban <alban.rouault@hotmail.com>
2023-06-18 08:05:27 +0200

    Suppression menu burger

--- src/view/element/HeaderEmploye.php
+++ src/view/element/HeaderEmploye.php
@@ -1,7 +1,7 @@
 <header class="header_client header_sticky">
     <nav class="nav_client">
         <ul>
-            <li><a href=""><i class="fa-solid fa-bars fa-2x header_icone"></i></a></li>
+            <!--<li><a href=""><i class="fa-solid fa-bars fa-2x header_icone"></i></a></li>-->
         </ul>
         <a href="<?php echo PUBLIC_FOLDER ?>"><img id="header_logo" src="<?= IMG ?>logo/logo.png"></a>
         <ul>
